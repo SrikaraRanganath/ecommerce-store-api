38e5d74eedee01fa9b667f171f0d5a5414cd9fbe
srikara ranganath <srikarar@gmail.com>
2023-01-03 13:34:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products</a>')
+})
+
+const productRouter = require('./routes/products');
+app.use('/api/v1/products', productRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5000;
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`Server is listening on port : ${PORT}...`))
+    } catch(error) {
+        console.log(error);
+    }
+}
+start();
